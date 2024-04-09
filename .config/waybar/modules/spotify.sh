2237da3d240c0e5170cfeec968976c020045d954
Sebastian Weddmark Olsson <visnae@gmail.com>
2024-04-09 11:40:49 +0200

    new waybar icons

--- .config/waybar/modules/spotify.sh
+++ .config/waybar/modules/spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
-icon=""
+icon="󰓇"
 
 if [[ $class == "playing" ]]; then
   info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
--- .config/waybar/modules/weather.sh
+++ .config/waybar/modules/weather.sh
@@ -37,73 +37,102 @@ temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]])+\.\./\1 to /g')
 
 declare -A conditions
 
-conditions["clear"]=""
-conditions["sunny"]=""
+# in Emacs C-x 8 [Enter] to insert unicode
 
-conditions["partly cloudy"]=""
+# F0590 weather-cloudy
+# F0F2F weather-cloudy-alert
+# F0E6E weather-cloudy-arrow-right
+# F18F6 weather-cloudy-clock
+# F0591 weather-fog
+# F0592 weather-hail
+# F0F30 weather-hazy
+# F0898 weather-hurricane
+# F0593 weather-lightning
+# F067E weather-lightning-rainy
+# F0594 weather-night
+# F0F31 weather-night-partly-cloudy
+# F0595 weather-partly-cloudy
+# F0F32 weather-partly-lightning
+# F0F33 weather-partly-rainy
+# F0F34 weather-partly-snowy
+# F0F35 weather-partly-snowy-rainy
+# F0596 weather-pouring
+# F0597 weather-rainy
+# F0598 weather-snowy
+# F0F36 weather-snowy-heavy
+# F067F weather-snowy-rainy
+# F0599 weather-sunny
+# F0F37 weather-sunny-alert
+# F14E4 weather-sunny-off
+# F059A weather-sunset
+# F059B weather-sunset-down
+# F059C weather-sunset-up
+# F0F38 weather-tornado
+# F059D weather-windy
+# F059E weather-windy-variant
 
-conditions["cloudy"]=""
-conditions["overcast"]=""
+# conditions taken from
+# https://github.com/chubin/wttr.in/blob/575908ae51010b2d71c0b1d4bf4def997bea0fa3/share/translations/en.txt
+#
+# Icons from Material Design
+# file://~/src/sources/MaterialDesign-Font/cheatsheet.html
 
-conditions["mist"]=""
-conditions["fog"]=""
-conditions["freezing fog"]=""
+# conditions["Clear"]=""
+conditions["Sunny"]="󰖙" # weather-sunny
+conditions["Partly cloudy"]="󰖕" # weather-partly-cloudy
+conditions["Cloudy"]="󰖐" # weather-cloudy
+conditions["Overcast"]="󱓤" # weather-sunny-off
+conditions["Mist"]="󰖑" # weather-fog
+conditions["Patchy rain possible"]="󰼳" # weather-partly-rainy
+conditions["Patchy snow possible"]="󰼴" # weather-partly-snowy
+# conditions["Patchy sleet possible"]=""
+# conditions["Patchy freezing drizzle possible"]=""
+# conditions["Thundery outbreaks possible"]=""
+# conditions["Blowing snow"]=""
+# conditions["Blizzard"]=""
+conditions["Fog"]="󰖑" # weather-fog
+conditions["Freezing fog"]="󰖑" # weather-fog
+# conditions["Patchy light drizzle"]=""
+# conditions["Light drizzle"]=""
+# conditions["Freezing drizzle"]=""
+# conditions["Heavy freezing drizzle"]=""
+conditions["Patchy light rain"]="󰼳" # weather-partly-rainy
+# conditions["Light rain"]=""
+# conditions["Moderate rain at times"]=""
+# conditions["Moderate rain"]=""
+# conditions["Heavy rain at times"]=""
+# conditions["Heavy rain"]=""
+# conditions["Light freezing rain"]=""
+# conditions["Moderate or heavy freezing rain"]=""
+# conditions["Light sleet"]=""
+# conditions["Moderate or heavy sleet"]=""
+# conditions["Patchy light snow"]=""
+# conditions["Light snow"]=""
+# conditions["Patchy moderate snow"]=""
+# conditions["Moderate snow"]=""
+# conditions["Patchy heavy snow"]=""
+conditions["Heavy snow"]="󰼶" # weather-snowy-heavy
+# conditions["Ice pellets"]=""
+conditions["Light rain shower"]="󰖗" # weather-rainy
+conditions["Moderate or heavy rain shower"]="󰖖" # weather-pouring
+# conditions["Torrential rain shower"]=""
+# conditions["Light sleet showers"]=""
+# conditions["Moderate or heavy sleet showers"]=""
+# conditions["Light snow showers"]=""
+# conditions["Moderate or heavy snow showers"]=""
+conditions["Patchy light rain with thunder"]="󰼲" # weather-partly-lightning
+conditions["Moderate or heavy rain with thunder"]="󰼱" # weather-lightning-rainy
+# conditions["Patchy light snow with thunder"]=""
+# conditions["Moderate or heavy snow with thunder"]=""
 
-conditions["patchy rain possible"]=""
-conditions["patchy light drizzle"]=""
-conditions["light drizzle"]=""
-conditions["patchy light rain"]=""
-conditions["light rain"]=""
-conditions["light rain shower"]=""
-conditions["rain"]=""
-
-conditions["moderate rain at times"]=""
-conditions["moderate rain"]=""
-conditions["heavy rain at times"]=""
-conditions["heavy rain"]=""
-conditions["moderate or heavy rain shower"]=""
-conditions["torrential rain shower"]=""
-conditions["rain shower"]=""
-
-conditions["patchy snow possible"]=""
-conditions["patchy sleet possible"]=""
-conditions["patchy freezing drizzle possible"]=""
-conditions["freezing drizzle"]=""
-conditions["heavy freezing drizzle"]=""
-conditions["light freezing rain"]=""
-conditions["moderate or heavy freezing rain"]=""
-conditions["light sleet"]=""
-conditions["ice pellets"]=""
-conditions["light sleet showers"]=""
-conditions["moderate or heavy sleet showers"]=""
-
-conditions["blowing snow"]=""
-conditions["moderate or heavy sleet"]=""
-conditions["patchy light snow"]=""
-conditions["light snow"]=""
-conditions["light snow showers"]=""
-
-conditions["blizzard"]="ｓ"
-conditions["patchy moderate snow"]="ｓ"
-conditions["moderate snow"]="ｓ"
-conditions["patchy heavy snow"]="ｓ"
-conditions["heavy snow"]="ｓ"
-conditions["moderate or heavy snow with thunder"]="ｓ"
-conditions["moderate or heavy snow showers"]="ｓ"
-
-conditions["thundery outbreaks possible"]=""
-conditions["patchy light rain with thunder"]=""
-conditions["moderate or heavy rain with thunder"]=""
-conditions["patchy light snow with thunder"]=""
-
-cond=$(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]')
+cond=$(echo ${weather[1]##*,})
 [ $DEBUG ] && echo "Condition $cond ${conditions[$cond]+exist}"
 
 if [ -n "${conditions[$cond]+exist}" ]
 then
     condition="${conditions[$cond]}"
 else
-    condition=""
+    condition="$cond"
 fi
 
 echo -e "{\"text\":\""$temperature $condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
